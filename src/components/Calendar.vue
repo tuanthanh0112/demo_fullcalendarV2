f66d0b42561b1d688c3850106b7e7c3aecde1cba
tuanthanh <marbled.thanh.tt@gmail.com>
2022-09-08 11:41:40 +0700

    delete vuex

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -18,7 +18,6 @@ import InteractionPlugin from "@fullcalendar/interaction";
 import ListPlugin from "@fullcalendar/list";
 import resourceTimelineDayPlugin from "@fullcalendar/resource-timeline";
 import { createEventId } from "../event/event-utils";
-import { mapActions } from "vuex";
 export default {
     name: "CalenDar",
     components: {
@@ -40,63 +39,45 @@ export default {
                     left:'prev,next', 
                 },
                 initialView: 'resourceTimelineDay',
-                scrollTime: '08:00',
+                scrollTime: '12:00',
                 aspectRatio: 1.5,
                 // initialEvents: INITIAL_EVENTS,
                 selectable: true,
                 height: 500,
-                dayMaxEvents: true,
-                weekends: false,
                 selectMirror: true,
                 select: this.handleDateSelect,
                 eventClick: this.handleEventClick,
-                eventsSet: this.handleEvents,
                 editable: true,
                 resourceAreaHeaderContent: 'không biết',
                 resources: 'https://fullcalendar.io/api/demo-feeds/resources.json?with-nesting&with-colors',
                 events: 'https://fullcalendar.io/api/demo-feeds/events.json?single-day&for-resource-timeline',
-                droppable: true,
-               
             },
             currentEvents:[]
         }
     },
     methods: {
-        ...mapActions([
-            'updateEvent'
-        ]),
-        handleWeekendsToggle() {
-             this.calendarPlugins.weekends = !this.calendarPlugins.weekends // update a property
-         },
-
+        
+        
         handleDateSelect(selectInfo) {
-            let title = prompt('Please enter a new title for your event')
+            let title = prompt('có thêm được đâu nà ní')
             let calendarApi = selectInfo.view.calendar
             calendarApi.unselect() // clear date selection
             if (title) {
                     calendarApi.addEvent({
                     id: createEventId(),
                     title,
-                    start: selectInfo.start,
-                    end: selectInfo.end,
-                    allTime: selectInfo.allTime
+                    start: selectInfo.startStr,
+                    end: selectInfo.endStr,
+                    allTime: selectInfo.allday
                 })
             }
         },
         handleEventClick(clickInfo) {
-            if(confirm(`'xoa di dung ngai' '${clickInfo.event.title}'`)){
+            if(confirm(`'xoa di ngai gi' '${clickInfo.event.title}'`)){
                 clickInfo.event.remove()
             }
         },
-        handleEvents(events) {
-            this.currentEvents = events
-        },
-        onEventDrop({event}){
-            return this.updateEvent(event)
-        }
-
-    
-
+       
     }
 }
 </script>
